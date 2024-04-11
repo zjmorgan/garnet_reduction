a6db8f5d25a93cd9dee323efc583ee226a385b36
Zachary Morgan (zgf) <zgf@discord.ornl.gov>
2024-04-11 15:51:40 -0400

    Add workflow

--- src/garnet/workflow.py
+++ src/garnet/workflow.py
@@ -12,26 +12,53 @@ from garnet.reduction.parallel import ParallelTasks
 from garnet.reduction.integration import Integration
 from garnet.reduction.normalization import Normalization
 
-filename, reduction, n_proc = sys.argv[1], sys.argv[2], int(sys.argv[3])
+filename, reduction, *args = sys.argv[1], sys.argv[2], sys.argv[3:]
+
+inst_dict = {'corell': 'CORELLI', 
+             'bl9': 'CORELLI',
+             'topaz': 'TOPAZ',
+             'bl12': 'TOPAZ',
+             'mandi': 'MANDI',
+             'bl11b': 'MANDI',
+             'snap': 'SNAP',
+             'bl3': 'SNAP',
+             'demand': 'DEMAND',
+             'hb3a': 'DEMAND',
+             'wand2': 'WAND²',
+             'hb2c': 'WAND²'}
+
+if type(args[0]) is int:
+    n_proc = int(args[0])
+else:
+    instrument = inst_dict[args[0].lower()]
+    filename = args[1]
+    assert filename.endswith('.json')
 
 if __name__ == '__main__':
 
     rp = ReductionPlan()
 
-    rp.load_plan(filename)
+    if reduction == 'temp':
 
-    if reduction == 'int':
-        func = Integration.integrate_parallel
-        comb = Integration.combine_parallel
-    elif reduction == 'norm':
-        func = Normalization.normalize_parallel
-        comb = Normalization.combine_parallel
+        rp.generate_plan(instrument)
+        rp.save_plan(os.path.abspath(filename))
 
-    pt = ParallelTasks(func, comb)
+    else:
 
-    max_proc = os.cpu_count()
+        rp.load_plan(filename)
 
-    if n_proc > max_proc:
-        n_proc = max_proc
+        if reduction == 'int':
+            func = Integration.integrate_parallel
+            comb = Integration.combine_parallel
+        elif reduction == 'norm':
+            func = Normalization.normalize_parallel
+            comb = Normalization.combine_parallel
 
-    pt.run_tasks(rp.plan, n_proc)
+        pt = ParallelTasks(func, comb)
+
+        max_proc = os.cpu_count()
+
+        if n_proc > max_proc:
+            n_proc = max_proc
+
+        pt.run_tasks(rp.plan, n_proc)
