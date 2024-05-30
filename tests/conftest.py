45995690fbdeb2c39bf3b49b63891bdb629f2a64
Patrou, Maria <patroum@ornl.gov>
2024-05-30 15:42:15 -0400

    test workflow temporary directory added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 """pytest config"""
 
-import pytest
 import os
+
+import pytest
 
 
 @pytest.fixture(scope="session")
@@ -9,11 +10,10 @@ def has_sns_mount():
     """Fixture that returns True if the SNS data mount (e.g CORELLI shared) is available"""
     sns_dir = "/SNS/CORELLI/shared/"
     return os.path.exists(sns_dir)
-     
+
 
 @pytest.fixture(scope="session")
 def has_hfir_mount():
     """Fixture that returns True if the HFIR data mount (e.g HB3A shared) is available"""
     hfir_dir = "/HFIR/HB3A/shared/"
     return os.path.exists(hfir_dir)
-     
