13d959d9ebaacab1164059bf593ded07066a3b84
Patrou, Maria <patroum@ornl.gov>
2024-06-03 15:20:36 -0400

    existing garnet-data repository is connected to this one for accessing test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,3 +31,17 @@ def has_hfir_mount():
     """Fixture that returns True if the HFIR data mount (e.g HB3A shared) is available"""
     hfir_dir = "/HFIR/HB3A/shared/"
     return os.path.exists(hfir_dir)
+
+
+@pytest.fixture(scope="session")
+def has_datarepo():
+    """Fixture that returns True if the datarepo_dir is available"""
+    readme_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "garnet-data", "README.md")
+    return os.path.exists(readme_data)
+
+
+@pytest.fixture(scope="session")
+def datarepo_dir():
+    """Return the directory **absolute** paths for test data."""
+    root_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "garnet-data")
+    return root_data
